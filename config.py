20d20b3a741f1186c7d1b4ed36d116d8c86812db
Monk Tea <71178046+MonkTeam@users.noreply.github.com>
2021-01-14 20:19:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,27 +3,27 @@ from pyrogram import filters
 from helpers import get_banned_users
 
 # Get these two from https://my.telegram.org
-API_ID = 2620155
-API_HASH = "7a8704afa675c39ec926782e798366a9"
+API_ID = 1760875
+API_HASH = "1cf39a758fd1abd6744561563f158ff3"
 
 # Get this from @Botfather
-TOKEN = "1514565679:AAHDWubNCdFiDZ-XTXaIy0NXpBozH95kMTM"
+TOKEN = "1566418454:AAHj1n8JqtQOSjLVmdy6EDLz8hjM2T6lFDc"
 
 # The IDs of the users which can stream, skip, pause and change volume
 SUDO_USERS = [
-1191438732,
-914472877
+1004538768,
+1327934109
 ]
 
 # A group ID to send messages to when a song starts playing
 # Example group ID: -1001402753006
-LOG_GROUP = None  # Just keep it like this if you are not going to use one
+LOG_GROUP = "-1001491078689"  # Just keep it like this if you are not going to use one
 
 # Choose the preferred language for your bot. If English leave as it is, or change to the code of any supported language.
 LANG = "en"
 
 # Max video duration allowed for downloads in minutes
-DUR_LIMIT = 5
+DUR_LIMIT = 10
 
 # Remove downloaded files after playing
 REMOVE_AFTER_PLAYING = False
